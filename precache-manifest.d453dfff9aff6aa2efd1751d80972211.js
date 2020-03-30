02b569482ba91f5d4f064bef6d9741e19163b894
rahulnag <rahulnag625@gmail.com>
2020-03-30 21:12:29 +0530

    Updates

--- precache-manifest.d453dfff9aff6aa2efd1751d80972211.js
+++ precache-manifest.d453dfff9aff6aa2efd1751d80972211.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c070df64019ce8dc2558eb2d1b91709f",
+    "revision": "2df04742fa2235a6922a0730cb8e49a2",
     "url": "/covid19/index.html"
   },
   {
-    "revision": "4c051a5c207b25244ae4",
+    "revision": "9485fe3764d2bf1b3e15",
     "url": "/covid19/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "70ef9b0b3afb21a670cf",
-    "url": "/covid19/static/js/2.0bc73278.chunk.js"
+    "revision": "0210c72de950b7c4961f",
+    "url": "/covid19/static/js/2.1cd39d3b.chunk.js"
   },
   {
-    "revision": "4c051a5c207b25244ae4",
-    "url": "/covid19/static/js/main.eae3779a.chunk.js"
+    "revision": "9485fe3764d2bf1b3e15",
+    "url": "/covid19/static/js/main.3d8ea549.chunk.js"
   },
   {
     "revision": "9edb58dfee5d88d19d35",
